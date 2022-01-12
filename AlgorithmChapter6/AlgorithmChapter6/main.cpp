5baec4a8bcf787d93d43acf101ffc23c860fc873
Hyeonyong16 <Hyeonyong16@users.noreply.github.com>
2022-01-13 04:32:12 +0900

    시계문제
    할당된 스위치에 맞춰 시계를 다 12시로 맞추는 최소횟수 문제

--- AlgorithmChapter6/AlgorithmChapter6/main.cpp
+++ AlgorithmChapter6/AlgorithmChapter6/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -41,7 +42,7 @@ public:
 			friendPair[inputFriendPair[(2 * i) + 1]][inputFriendPair[2 * i]] = true;
 		}
 	}
-	
+
 	int getStudentNum() {
 		return  studentNum;
 	}
@@ -80,7 +81,7 @@ public:
 	}
 };
 
-int pairingFriends(bool check[10], inputData& data) {
+int PairingFriends(bool check[10], inputData& data) {
 	int frontNum = -1;	//현재 짝을 확인해야할 학생 (-1이면 없는것)
 
 	for (int i = 0; i < data.getStudentNum(); i++) {
@@ -91,14 +92,14 @@ int pairingFriends(bool check[10], inputData& data) {
 	}
 
 	if (frontNum == -1) return 1;
-	bool (*friendPair)[10] = data.getFriendPair();
+	bool(*friendPair)[10] = data.getFriendPair();
 	int result = 0;
 
 	for (int i = frontNum + 1; i < data.getStudentNum(); i++) {
 		if (!check[i] && friendPair[frontNum][i]) {
 			check[frontNum] = true;
 			check[i] = true;
-			result += pairingFriends(check, data);
+			result += PairingFriends(check, data);
 			check[frontNum] = false;
 			check[i] = false;
 		}
@@ -108,9 +109,61 @@ int pairingFriends(bool check[10], inputData& data) {
 }
 
 //6.8문제 시계 맞추기
+const bool switches[10][16] = {
+		{1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},	//0
+		{0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0},	//1
+		{0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1},	//2
+		{1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0},	//3
+		{0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0},	//4
+		{1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1},	//5
+		{0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1},	//6
+		{0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1},	//7
+		{0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},	//8
+		{0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0}	//9
+};
+
+void PushSwitch(vector<int>& clocks, int activeSwitch) {
+	for (int i = 0; i < 16; i++) {
+		if (switches[activeSwitch][i]) {
+			clocks[i] += 3;
+			if (clocks[i] == 15) clocks[i] = 3;
+		}
+	}
+}
+
+bool IsClockMatched(vector<int>& clocks) {
+	bool checked = true;
+	for (int i = 0; i < 16; i++) {
+		if (clocks[i] != 12) {
+			checked = false;
+			break;
+		}
+	}
+
+	return checked;
+}
+
+int FindSolution(vector<int>& clocks, int curSwitch) {
+	if (curSwitch == 10) {
+		bool temp;
+		temp = IsClockMatched(clocks);
+		if (temp) return 0;
+		else return 9999;
+	}
+
+	int val = 99999;
+	for (int i = 0; i < 4; i++) {
+		val = min(val, i + FindSolution(clocks, curSwitch + 1));
+		PushSwitch(clocks, curSwitch);
+	}
+
+	return val;
+}
 
 int main() {
-	int question = 1;
+	int question = 2;
+
+	//6.3 소풍
 	if (question == 0) {
 
 		int testCaseNum = 0;			//입력받을 테스트 케이스 숫자
@@ -131,13 +184,29 @@ int main() {
 
 		for (int i = 0; i < testCaseNum; i++) {
 			data.dataInput(inputFriendPair);
-			cout << "방법의 수: " << pairingFriends(pairCheck, data) << endl;
+			cout << "방법의 수: " << PairingFriends(pairCheck, data) << endl;
 			data.initData();
 			inputFriendPair.clear();
 		}
 	}
 
-	else if (question == 1) {
+	//6.8 시계 맞추기
+	else if (question == 2) {
+		int testCaseNum = 0;
+		int input;
 
+		vector<int> clocks;
+
+		cin >> testCaseNum;
+		cin.clear();
+
+		for (int i = 0; i < 16; i++) {
+			cin >> input;
+			cin.clear();
+
+			clocks.push_back(input);
+		}
+
+		cout << "최소 회수: " << FindSolution(clocks, 0) << endl;
 	}
 }
